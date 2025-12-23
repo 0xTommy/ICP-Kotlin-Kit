b4ff450fa94b438568ba8cb650118203b2cdb8fa
thomasconstantin <thomas.constantin@bity.com>
2025-12-23 14:58:10 +0100

    feature: FetchTokenTransactions returns result

--- icp_kotlin_kit/src/main/java/com/bity/icp_kotlin_kit/domain/use_case/transaction/FetchAccountTransactions.kt
+++ icp_kotlin_kit/src/main/java/com/bity/icp_kotlin_kit/domain/use_case/transaction/FetchAccountTransactions.kt
@@ -1,10 +1,8 @@
 package com.bity.icp_kotlin_kit.domain.use_case.transaction
 
-import com.bity.icp_kotlin_kit.domain.exception.ICPKitException
 import com.bity.icp_kotlin_kit.domain.model.ICPAccount
 import com.bity.icp_kotlin_kit.domain.model.token_transaction.ICPTokenTransaction
 import com.bity.icp_kotlin_kit.domain.use_case.token.FetchAllTokens
-import com.bity.icp_kotlin_kit.util.logger.ICPKitLogger
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
@@ -26,18 +24,10 @@ class FetchAccountTransactions internal constructor(
         coroutineScope {
             tokens.map { token ->
                 async {
-                    try {
-                        fetchTokenTransactions(
-                            account = account,
-                            token = token
-                        )
-                    } catch (ex: ICPKitException) {
-                        ICPKitLogger.logError(
-                            "Unable to get transactions for token ${token.name}",
-                            ex
-                        )
-                        null
-                    }
+                    fetchTokenTransactions(
+                        account = account,
+                        token = token
+                    ).getOrNull()
                 }
             }.forEach { deferred ->
                 deferred.await()?.let { emit(it) }
--- icp_kotlin_kit/src/main/java/com/bity/icp_kotlin_kit/domain/use_case/transaction/FetchTokenTransactions.kt
+++ icp_kotlin_kit/src/main/java/com/bity/icp_kotlin_kit/domain/use_case/transaction/FetchTokenTransactions.kt
@@ -5,6 +5,7 @@ import com.bity.icp_kotlin_kit.domain.factory.TransactionRepositoryFactory
 import com.bity.icp_kotlin_kit.domain.model.ICPAccount
 import com.bity.icp_kotlin_kit.domain.model.ICPToken
 import com.bity.icp_kotlin_kit.domain.model.token_transaction.ICPTokenTransaction
+import com.bity.icp_kotlin_kit.util.logger.ICPKitLogger
 
 class FetchTokenTransactions internal constructor(
     private val transactionRepositoryFactory: TransactionRepositoryFactory
@@ -13,10 +14,19 @@ class FetchTokenTransactions internal constructor(
     suspend operator fun invoke(
         account: ICPAccount,
         token: ICPToken
-    ): List<ICPTokenTransaction> {
+    ): Result<List<ICPTokenTransaction>> {
         val repository = transactionRepositoryFactory.getTransactionRepository(token)
-            ?: throw ICPKitException.TokenNotSupported(token)
-        return repository.fetchAllTransactions(account)
+            ?: return Result.failure(ICPKitException.TokenNotSupported(token))
+        return try {
+            val transactions = repository.fetchAllTransactions(account)
+            Result.success(transactions)
+        } catch (t: Throwable) {
+            ICPKitLogger.logError(
+                "Failed to fetch transactions for account $account and token $token",
+                t
+            )
+            Result.failure(t)
+        }
     }
 
 }
