03e56549bcf7e3e87a1d184e7d3626f1c6b21eed
thomasconstantin <thomas.constantin@bity.com>
2025-12-23 18:27:07 +0100

    feature: FetchTokenTransactionFee return Result

--- icp_kotlin_kit/src/main/java/com/bity/icp_kotlin_kit/domain/use_case/token/FetchTokenTransactionFee.kt
+++ icp_kotlin_kit/src/main/java/com/bity/icp_kotlin_kit/domain/use_case/token/FetchTokenTransactionFee.kt
@@ -3,18 +3,28 @@ package com.bity.icp_kotlin_kit.domain.use_case.token
 import com.bity.icp_kotlin_kit.domain.factory.TokenRepositoryFactory
 import com.bity.icp_kotlin_kit.domain.model.ICPToken
 import com.bity.icp_kotlin_kit.domain.repository.TokenRepository
+import com.bity.icp_kotlin_kit.util.logger.ICPKitLogger
 import java.math.BigInteger
 
 class FetchTokenTransactionFee internal constructor(
     private val tokenRepositoryFactory: TokenRepositoryFactory
 ) {
 
-    suspend operator fun invoke(token: ICPToken): BigInteger {
+    suspend operator fun invoke(token: ICPToken): Result<BigInteger> {
         val repository = tokenRepositoryFactory.createRepository(
             standard = token.standard,
             canister = token.canister
         )
-        return repository.fee()
+        return try {
+            val fee = repository.fee()
+            Result.success(fee)
+        } catch (t: Throwable) {
+            ICPKitLogger.logError(
+                "Failed to fetch transaction fee for token ${token.canister}",
+                t
+            )
+            Result.failure(t)
+        }
     }
 
 }
